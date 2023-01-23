91350860f2b6b0debf329e5393ea0f6d2cd7d9d5
GhostMaster69-dev <rathore6375@gmail.com>
2023-01-23 14:21:34 +0530

    Test sepolicy
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,7 @@
-#! /bin/bash
+# Rom Dir
+cd ~/octavi
 
-# Setup git
-git config --global user.name GhostMaster69-dev
-git config --global user.email rathore6375@gmail.com
-
-# Rom dir
-mkdir ~/octavi && cd ~/octavi
-
-# sync rom
-repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen
-git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
-repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
-
-# build rom
+# Make Rom
 source build/envsetup.sh
 lunch octavi_vince-user
 mka sepolicy -j$(nproc --all)
---
+++ sync.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Rom dir
+mkdir ~/octavi
+cd ~/octavi
+
+# sync rom
+repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen
+git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
+repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
