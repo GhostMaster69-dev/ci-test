ec6dcbc9e683d781302b0117a431e741ca02c702
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-20 20:00:07 +0530

    Revert "Push existing project to GitLab"
    This reverts commit 440251dbb2830a9d67d255ad006ec44beeaae706.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- push.sh
+++
@@ -1,8 +0,0 @@
-cd ~/octavi
-
-# Push to GitLab
-git init
-git add .
-git commit -q -m "Push existing octavi source to GitLab"
-git remote add source https://gitlab.com/GhostMaster69-dev/octavi-source
-git push -q source master
