c24ba3f30fcf418c7602936c0da789bc6733531b
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-21 22:38:51 +0530

    Make sepolicy
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- sync.sh
+++ sync.sh
@@ -7,4 +7,3 @@ cd ~/octavi
 repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen
 git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b 13 .repo/local_manifests
 repo sync --no-clone-bundle --no-tags -j$(nproc --all)
-#git clone https://github.com/GhostMaster69-dev/android_device_xiaomi_vince.git -b 13 device/xiaomi/vince
