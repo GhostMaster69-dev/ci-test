c68b935766e63caf4ab06489a87ded6c68520060
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-23 10:17:34 +0530

    Make bootimage
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- sync.sh
+++ sync.sh
@@ -7,3 +7,4 @@ cd ~/octavi
 repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen
 git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b 13 .repo/local_manifests
 repo sync --no-clone-bundle --no-tags -j$(nproc --all)
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
