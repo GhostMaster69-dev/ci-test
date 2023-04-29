e5f210abfe990962cc2c052b2d561bf3e6e79111
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-29 05:38:25 +0530

    Make sepolicy
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ cd ~/arrow
 # Make Rom
 . build/envsetup.sh
 lunch arrow_vince-user
-mka bootimage -j$(nproc --all)
-#mka sepolicy -j$(nproc --all)
+#mka bootimage -j$(nproc --all)
+mka sepolicy -j$(nproc --all)
+mma ParanoidDoze
 
