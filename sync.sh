6bf41d47fc6ba3563f8e8ceeb94fd32a661eb4b0
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-30 22:15:57 +0530

    Test sepolicy
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- sync.sh
+++ sync.sh
@@ -5,9 +5,5 @@ cd ~/octavi
 
 # sync rom
 repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen
-git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b 13 .repo/local_manifests
-# Remove existing dir before sync
-if [ -d prebuilts/clang/host/linux-x86 ]; then
-    find prebuilts/clang/host/linux-x86 -delete
-fi
-repo sync --no-clone-bundle --no-tags -j$(nproc --all)
+git clone https://github.com/GhostMaster69-dev/local_manifest.git -b 13 .repo/local_manifests
+repo sync --no-clone-bundle --no-tags --current-branch --force-sync --force-remove-dirty --jobs=$(nproc --all) --jobs-checkout=$(nproc --all) --jobs-network=$(nproc --all)
