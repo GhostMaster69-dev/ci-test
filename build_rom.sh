0f98be4169674be20fef52add188460472edde4f
GhostMaster69-dev <rathore6375@gmail.com>
2023-07-14 22:27:45 +0530

    Test bootimg

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,8 @@ cd ~/octavi
 # Make Rom
 source build/envsetup.sh
 lunch octavi_vince-user
-mma Settings -j$(nproc --all)
-#mka bootimage -j$(nproc --all)
+#mma Settings -j$(nproc --all)
+mka bootimage -j$(nproc --all)
 #mka sepolicy -j$(nproc --all)
 #mma ParanoidDoze -j$(nproc --all)
 
