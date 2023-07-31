11c1912d72f807b8e6cf079043683a9bf7a9f7f3
GhostMaster69-dev <rathore6375@gmail.com>
2023-07-31 19:23:41 +0530

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -9,3 +9,4 @@ mka bootimage -j$(nproc --all)
 #mka sepolicy -j$(nproc --all)
 #mma ParanoidDoze -j$(nproc --all)
 
+gh release upload 'Boot Image' ~/octavi/out/target/product/vince/boot.img -R github.com/AOSP-Silicon/kernel_xiaomi_vince
