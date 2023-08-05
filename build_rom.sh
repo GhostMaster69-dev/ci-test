e7fcedea4ebadf826b09137e084cf6b13b353b72
GhostMaster69-dev <rathore6375@gmail.com>
2023-08-05 08:25:07 +0530

    test

--- build_rom.sh
+++ build_rom.sh
@@ -9,4 +9,5 @@ mka bootimage -j$(nproc --all)
 #mka sepolicy -j$(nproc --all)
 #mma ParanoidDoze -j$(nproc --all)
 
-gh release upload 'Boot Image' ~/octavi/out/target/product/vince/boot.img -R github.com/AOSP-Silicon/kernel_xiaomi_vince
+gh release create v$(date +%Y%m%d | cut -c 4- | sed "s/0/./g") --latest -n "What's New ($(date +"%d %B %Y")):" --generate-notes -t Release -R AOSP-Silicon/kernel_xiaomi_vince > /dev/null 2>&1
+gh release upload v$(date +%Y%m%d | cut -c 4- | sed "s/0/./g") ~/octavi/out/target/product/vince/boot.img -R AOSP-Silicon/kernel_xiaomi_vince
